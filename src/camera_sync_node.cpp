26353e996a4dee0ac41db823bf5d8f2b06824f31
azraav <avally02@gmail.com>
2023-07-11 12:51:50 +0200

    edits

--- src/camera_sync_node.cpp
+++ src/camera_sync_node.cpp
@@ -46,40 +46,28 @@ public:
     
     // Initialise subscribers to the topics of interest
     boson_sub_.subscribe(this, "/boson/image_raw", rmw_qos_profile);
-    //lepton_sub_.subscribe(this, "/lepton1/lepton/image_raw", rmw_qos_profile);
     realsense_rgb_sub_.subscribe(this, "/camera/color/image_raw", rmw_qos_profile);
     realsense_depth_sub_.subscribe(this, "/camera/depth/image_rect_raw", rmw_qos_profile);
-
-    //ximea_sub_.subscribe(this, "/ximea/image_raw", rmw_qos_profile);
-    //realsense_infra1_sub_.subscribe(this, "/camera/infra1/image_rect_raw", rmw_qos_profile);
-    //realsense_infra2_sub_.subscribe(this, "/camera/infra2/image_rect_raw", rmw_qos_profile);
+    realsense_infra1_sub_.subscribe(this, "/camera/infra1/image_rect_raw", rmw_qos_profile);
+    realsense_infra2_sub_.subscribe(this, "/camera/infra2/image_rect_raw", rmw_qos_profile);
 
 
     // Individual subscribers
     boson_sub_.registerCallback(&CameraSynchroniserNode::boson_callback, this);
-    //lepton_sub_.registerCallback(&CameraSynchroniserNode::lepton_callback, this);
     realsense_rgb_sub_.registerCallback(&CameraSynchroniserNode::realsense_rgb_callback, this);
     realsense_depth_sub_.registerCallback(&CameraSynchroniserNode::realsense_depth_callback, this);
-
-    //ximea_sub_.registerCallback(&CameraSynchroniserNode::ximea_callback, this);
-    //realsense_infra1_sub_.registerCallback(&CameraSynchroniserNode::realsense_infra1_callback, this);
-    //realsense_infra2_sub_.registerCallback(&CameraSynchroniserNode::realsense_infra2_callback, this);
+    realsense_infra1_sub_.registerCallback(&CameraSynchroniserNode::realsense_infra1_callback, this);
+    realsense_infra2_sub_.registerCallback(&CameraSynchroniserNode::realsense_infra2_callback, this);
     
 
     
     
     // Create an approximate time filter
-    syncApproximate.reset(new ApproxSync(approximate_policy(100), realsense_rgb_sub_, realsense_depth_sub_, boson_sub_));  
+    syncApproximate.reset(new ApproxSync(approximate_policy(100), realsense_rgb_sub_, realsense_depth_sub_, boson_sub_, realsense_infra1_sub_, realsense_infra2_sub_));  
 
     // Register the approximate time callback.
     syncApproximate->registerCallback(&CameraSynchroniserNode::approximate_sync_callback, this);
     syncApproximate->setMaxIntervalDuration(rclcpp::Duration(0,10000000)); 
-    
-
-    // Publish synced image messages  
-    //this->publisher_image_raw_synced_   = this->create_publisher<sensor_msgs::msg::Image>("synced_image", qos);
-    //this->publisher_thermal_raw_synced_ = this->create_publisher<flir_lepton_msgs::msg::TemperatureRaw>("synced_thermal", qos);
-
     }
  
 private:
@@ -88,54 +76,8 @@ private:
     void boson_callback(const sensor_msgs::msg::Image::SharedPtr &boson_msg)
     {
         //RCLCPP_INFO(this->get_logger(), "I heard the following BOSON message: %u. %u", boson_msg->header.stamp.sec, boson_msg->header.stamp.nanosec);
-        // Convert RAW16 data to 8 bits to show as ROS2 Image msg
-        //this->thermal16 = cv::Mat(thermal_img_msg->height, thermal_img_msg->width, CV_16U, &thermal_img_msg->data[0]);
-        //normalize(this->thermal16, this->thermal8, 0, 65535, cv::NORM_MINMAX);
-        //this->thermal8.convertTo(this->thermal8,CV_8U,1./256.);
-        
-        //cv::bitwise_not(this->thermal8, this->thermal8);
-        //std::string timestr = std::to_string(thermal_img_msg->header.stamp.sec) + std::to_string(thermal_img_msg->header.stamp.nanosec);
-           
-        //std::string img_name_thermal = ("/home/orin/m2s2_ws/cam_calib_parameters/boson_ximea/boson_imgs/" + timestr + ".png");
-        //std::string img_name_thermal_raw = ("/home/orin/m2s2_ws/cam_calib_parameters/boson_ximea/boson_raw_imgs/" + timestr + ".png");
-        
-        //cv::imwrite(img_name_thermal, this->thermal8);
-        //cv::imwrite(img_name_thermal_raw, this->thermal16);
-
-        
     }
 
-    /*
-    void ximea_callback(const sensor_msgs::msg::Image::SharedPtr &ximea_msg)
-    {
-        //RCLCPP_INFO(this->get_logger(), "I heard the following XIMEA message: %u. %u", ximea_msg->header.stamp.sec, ximea_msg->header.stamp.nanosec);
-
-        //write to image using opencv
-        //cv_bridge::CvImagePtr cv_ptr_rgb, cv_ptr_thermal;
-        //cv_ptr_rgb = cv_bridge::toCvCopy(rgb_img_msg, rgb_img_msg->encoding);
-        //std::string timestr = std::to_string(rgb_img_msg->header.stamp.sec) + std::to_string(rgb_img_msg->header.stamp.nanosec);
-
-        //std::string img_name_rgb = ("/home/orin/m2s2_ws/cam_calib_parameters/boson_ximea/ximea_imgs/" + timestr + ".png");
-        //cv::imwrite(img_name_rgb, cv_ptr_rgb->image);
-
-
-
-    }
-    */
-
-    
-    /*void lepton_callback(const sensor_msgs::msg::Image::SharedPtr &lepton_msg)
-    {
-        //RCLCPP_INFO(this->get_logger(), "I heard the following LEPTON message: %u. %u", lepton_msg->header.stamp.sec, lepton_msg->header.stamp.nanosec);
-
-        //write to image using opencv
-        //cv_bridge::CvImagePtr cv_ptr_rgb, cv_ptr_thermal;
-        //cv_ptr_rgb = cv_bridge::toCvCopy(rgb_img_msg, rgb_img_msg->encoding);
-        //std::string timestr = std::to_string(rgb_img_msg->header.stamp.sec) + std::to_string(rgb_img_msg->header.stamp.nanosec);
-
-        //std::string img_name_rgb = ("/home/orin/m2s2_ws/cam_calib_parameters/boson_ximea/ximea_imgs/" + timestr + ".png");
-        //cv::imwrite(img_name_rgb, cv_ptr_rgb->image);
-    }*/
 
     void realsense_rgb_callback(const sensor_msgs::msg::Image::SharedPtr &realsense_rgb_msg)
     {
@@ -149,7 +91,7 @@ private:
 
     }
 
-    /*void realsense_infra1_callback(const sensor_msgs::msg::Image::SharedPtr &realsense_infra1_msg)
+    void realsense_infra1_callback(const sensor_msgs::msg::Image::SharedPtr &realsense_infra1_msg)
     {
         //RCLCPP_INFO(this->get_logger(), "I heard the following RealSense Infra1 message: %u. %u", realsense_infra1_msg->header.stamp.sec, realsense_infra1_msg->header.stamp.nanosec);
 
@@ -159,80 +101,52 @@ private:
     {
         //RCLCPP_INFO(this->get_logger(), "I heard the following RealSense Infra2 message: %u. %u", realsense_infra2_msg->header.stamp.sec, realsense_infra2_msg->header.stamp.nanosec);
 
-    }*/
+    }
 
 
-    void approximate_sync_callback(const sensor_msgs::msg::Image::SharedPtr &realsense_rgb_msg, const sensor_msgs::msg::Image::SharedPtr &realsense_depth_msg, const sensor_msgs::msg::Image::SharedPtr &boson_msg)
+    void approximate_sync_callback(const sensor_msgs::msg::Image::SharedPtr &realsense_rgb_msg, const sensor_msgs::msg::Image::SharedPtr &realsense_depth_msg, const sensor_msgs::msg::Image::SharedPtr &boson_msg, const sensor_msgs::msg::Image::SharedPtr &realsense_infra1_msg, const sensor_msgs::msg::Image::SharedPtr &realsense_infra2_msg)
     {   
 
         RCLCPP_INFO(this->get_logger(), "Received APPROXIMATE msgs");
-
-        // Convert RAW16 data to 8 bits to show as ROS2 Image msg
-        //this->thermal16 = cv::Mat(thermal_img_msg->height, thermal_img_msg->width, CV_16U, &thermal_img_msg->data[0]);
-        //normalize(this->thermal16, this->thermal8, 0, 65535, cv::NORM_MINMAX);
-        //this->thermal8.convertTo(this->thermal8,CV_8U,1./256.);
-        //cv::bitwise_not(this->thermal8, this->thermal8);
-        
+    
         // write to image using opencv
-        cv_bridge::CvImagePtr cv_ptr_rs_rgb, cv_ptr_rs_depth, cv_ptr_boson;
+        cv_bridge::CvImagePtr cv_ptr_rs_rgb, cv_ptr_rs_depth, cv_ptr_boson, cv_ptr_rs_infra1, cv_ptr_rs_infra2;
         cv_ptr_boson = cv_bridge::toCvCopy(boson_msg, boson_msg->encoding);
-        //cv_ptr_lepton = cv_bridge::toCvCopy(lepton_msg, lepton_msg->encoding);
         cv_ptr_rs_rgb = cv_bridge::toCvCopy(realsense_rgb_msg, realsense_rgb_msg->encoding);
         cv_ptr_rs_depth = cv_bridge::toCvCopy(realsense_depth_msg, realsense_depth_msg->encoding);
-        
-        //cv_ptr_ximea = cv_bridge::toCvCopy(ximea_msg, ximea_msg->encoding);
-        //cv_ptr_rs_infra1 = cv_bridge::toCvCopy(realsense_infra1_msg, realsense_infra1_msg->encoding);
-        //cv_ptr_rs_infra2 = cv_bridge::toCvCopy(realsense_infra2_msg, realsense_infra2_msg->encoding);
+        cv_ptr_rs_infra1 = cv_bridge::toCvCopy(realsense_infra1_msg, realsense_infra1_msg->encoding);
+        cv_ptr_rs_infra2 = cv_bridge::toCvCopy(realsense_infra2_msg, realsense_infra2_msg->encoding);
 
+        std::string path = "/home/m2s2/Calibration_Data/20230605/boson_rs/";
 
-
-
-
-        std::string img_name_boson = ("/home/orin/m2s2_ws/Experiments/20230509/boson_realsense/boson/" + std::to_string(count) + ".png");
-        //std::string img_name_lepton = ("/home/orin/m2s2_ws/Experiments/20230426/lepton/" + std::to_string(count) + ".png");
-        std::string img_name_rs_rgb = ("/home/orin/m2s2_ws/Experiments/20230509/boson_realsense/realsense_rgb/" + std::to_string(count) + ".png");        //std::string img_name_ximea = ("/home/orin/m2s2_ws/Experiments/20230426/ximea/" + std::to_string(count) + ".png");
-        std::string img_name_rs_depth = ("/home/orin/m2s2_ws/Experiments/20230509/boson_realsense/realsense_depth/" + std::to_string(count) + ".png");        //std::string img_name_ximea = ("/home/orin/m2s2_ws/Experiments/20230426/ximea/" + std::to_string(count) + ".png");
-
-        //std::string img_name_ximea = ("/home/orin/m2s2_ws/Experiments/20230426/ximea/" + std::to_string(count) + ".png");
-        //std::string img_name_rs_infra1 = ("/home/orin/m2s2_ws/Experiments/20230426_realsense/rs_infra1/" + std::to_string(count) + ".png");
-        //std::string img_name_rs_infra2 = ("/home/orin/m2s2_ws/Experiments/20230426_realsense/rs_infra2/" + std::to_string(count) + ".png");
+        std::string img_name_boson =     (path + "boson/" + std::to_string(count) + ".png");
+        std::string img_name_rs_rgb =    (path + "rs_rgb/" + std::to_string(count) + ".png");        
+        std::string img_name_rs_depth =  (path + "rs_depth/" + std::to_string(count) + ".png");        
+        std::string img_name_rs_infra1 = (path + "rs_infra1/" + std::to_string(count) + ".png");
+        std::string img_name_rs_infra2 = (path + "rs_infra2/" + std::to_string(count) + ".png");
 
         cv::imwrite(img_name_boson, cv_ptr_boson->image);
-        //cv::imwrite(img_name_lepton, cv_ptr_lepton->image);
         cv::imwrite(img_name_rs_rgb, cv_ptr_rs_rgb->image);
         cv::imwrite(img_name_rs_depth, cv_ptr_rs_depth->image);
-
-
-        //cv::imwrite(img_name_ximea, cv_ptr_ximea->image);
-        //cv::imwrite(img_name_rs_infra1, cv_ptr_rs_infra1->image);
-        //cv::imwrite(img_name_rs_infra2, cv_ptr_rs_infra2->image);
-            
-
-    
-        
+        cv::imwrite(img_name_rs_infra1, cv_ptr_rs_infra1->image);
+        cv::imwrite(img_name_rs_infra2, cv_ptr_rs_infra2->image);
     
         count = count+1;
-
-
     }
 
     // Subscribers to individual topics
     message_filters::Subscriber<sensor_msgs::msg::Image> boson_sub_;
-    //message_filters::Subscriber<sensor_msgs::msg::Image> lepton_sub_;
     message_filters::Subscriber<sensor_msgs::msg::Image> realsense_rgb_sub_;
     message_filters::Subscriber<sensor_msgs::msg::Image> realsense_depth_sub_;
-
-    //message_filters::Subscriber<sensor_msgs::msg::Image> ximea_sub_;
-    //message_filters::Subscriber<sensor_msgs::msg::Image> realsense_infra1_sub_;
-    //message_filters::Subscriber<sensor_msgs::msg::Image> realsense_infra2_sub_;
+    message_filters::Subscriber<sensor_msgs::msg::Image> realsense_infra1_sub_;
+    message_filters::Subscriber<sensor_msgs::msg::Image> realsense_infra2_sub_;
 
 
     // Publishers for synced image messages
     //rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr publisher_image_raw_synced_;
-    //rclcpp::Publisher<flir_lepton_msgs::msg::TemperatureRaw>::SharedPtr publisher_thermal_raw_synced_;
 
     // Message filter stuff
-    typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::msg::Image, sensor_msgs::msg::Image, sensor_msgs::msg::Image> approximate_policy;
+    typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::msg::Image, sensor_msgs::msg::Image, sensor_msgs::msg::Image, sensor_msgs::msg::Image, sensor_msgs::msg::Image> approximate_policy;
     typedef message_filters::Synchronizer<approximate_policy> ApproxSync;
     std::shared_ptr<ApproxSync> syncApproximate;
 
--- src/camera_sync_node_lepton.cpp
+++ src/camera_sync_node_lepton.cpp
@@ -45,25 +45,35 @@ public:
    
     
     // Initialise subscribers to the topics of interest
-    lepton_sub_.subscribe(this, "/lepton1/lepton/image_raw", rmw_qos_profile);
+    lepton1_sub_.subscribe(this, "/lepton1/lepton/image_raw", rmw_qos_profile);
+    lepton2_sub_.subscribe(this, "/lepton2/lepton2/image_raw", rmw_qos_profile);
     realsense_rgb_sub_.subscribe(this, "/camera/color/image_raw", rmw_qos_profile);
-    realsense_depth_sub_.subscribe(this, "/camera/depth/image_rect_raw", rmw_qos_profile);
+    //realsense_depth_sub_.subscribe(this, "/camera/depth/image_rect_raw", rmw_qos_profile);
+    ximea_sub_.subscribe(this, "/ximea/image_raw", rmw_qos_profile);
+    realsense_infra1_sub_.subscribe(this, "/camera/infra1/image_rect_raw", rmw_qos_profile);
+    realsense_infra2_sub_.subscribe(this, "/camera/infra2/image_rect_raw", rmw_qos_profile);
+    boson_sub_.subscribe(this, "/boson/image_raw", rmw_qos_profile);
 
    
 
     // Individual subscribers
-    lepton_sub_.registerCallback(&CameraLeptonSynchroniserNode::lepton_callback, this);
+    lepton1_sub_.registerCallback(&CameraLeptonSynchroniserNode::lepton1_callback, this);
+    lepton2_sub_.registerCallback(&CameraLeptonSynchroniserNode::lepton2_callback, this);
     realsense_rgb_sub_.registerCallback(&CameraLeptonSynchroniserNode::realsense_rgb_callback, this);
-    realsense_depth_sub_.registerCallback(&CameraLeptonSynchroniserNode::realsense_depth_callback, this);
+    //realsense_depth_sub_.registerCallback(&CameraLeptonSynchroniserNode::realsense_depth_callback, this);
+    ximea_sub_.registerCallback(&CameraLeptonSynchroniserNode::ximea_callback, this);
+    realsense_infra1_sub_.registerCallback(&CameraLeptonSynchroniserNode::realsense_infra1_callback, this);
+    realsense_infra2_sub_.registerCallback(&CameraLeptonSynchroniserNode::realsense_infra2_callback, this);
+    boson_sub_.registerCallback(&CameraLeptonSynchroniserNode::boson_callback, this);
 
     
     
     // Create an approximate time filter
-    syncApproximate.reset(new ApproxSync(approximate_policy(100), realsense_rgb_sub_, realsense_depth_sub_, lepton_sub_));  
+    syncApproximate.reset(new ApproxSync(approximate_policy(100), boson_sub_, realsense_rgb_sub_, ximea_sub_, lepton1_sub_, lepton2_sub_, realsense_infra1_sub_, realsense_infra2_sub_));  
 
     // Register the approximate time callback.
     syncApproximate->registerCallback(&CameraLeptonSynchroniserNode::approximate_sync_callback, this);
-    syncApproximate->setMaxIntervalDuration(rclcpp::Duration(0,10000000)); 
+    syncApproximate->setMaxIntervalDuration(rclcpp::Duration(0,100000000)); 
 
     }
  
@@ -71,7 +81,12 @@ private:
     
 
     
-    void lepton_callback(const sensor_msgs::msg::Image::SharedPtr &lepton_msg)
+    void lepton1_callback(const sensor_msgs::msg::Image::SharedPtr &lepton1_msg)
+    {
+        //RCLCPP_INFO(this->get_logger(), "I heard the following LEPTON message: %u. %u", lepton_msg->header.stamp.sec, lepton_msg->header.stamp.nanosec);  
+    }
+
+    void lepton2_callback(const sensor_msgs::msg::Image::SharedPtr &lepton2_msg)
     {
         //RCLCPP_INFO(this->get_logger(), "I heard the following LEPTON message: %u. %u", lepton_msg->header.stamp.sec, lepton_msg->header.stamp.nanosec);  
     }
@@ -81,46 +96,100 @@ private:
         //RCLCPP_INFO(this->get_logger(), "I heard the following RealSense RGB message: %u. %u", realsense_rgb_msg->header.stamp.sec, realsense_rgb_msg->header.stamp.nanosec);
     }
     
-   void realsense_depth_callback(const sensor_msgs::msg::Image::SharedPtr &realsense_depth_msg)
+   /*void realsense_depth_callback(const sensor_msgs::msg::Image::SharedPtr &realsense_depth_msg)
+    {
+        //RCLCPP_INFO(this->get_logger(), "I heard the following RealSense Depth message: %u. %u", realsense_depth_msg->header.stamp.sec, realsense_depth_msg->header.stamp.nanosec);
+    }*/
+
+    void ximea_callback(const sensor_msgs::msg::Image::SharedPtr &ximea_msg)
     {
         //RCLCPP_INFO(this->get_logger(), "I heard the following RealSense Depth message: %u. %u", realsense_depth_msg->header.stamp.sec, realsense_depth_msg->header.stamp.nanosec);
     }
 
-    void approximate_sync_callback(const sensor_msgs::msg::Image::SharedPtr &realsense_rgb_msg, const sensor_msgs::msg::Image::SharedPtr &realsense_depth_msg, const sensor_msgs::msg::Image::SharedPtr &lepton_msg)
+    void realsense_infra1_callback(const sensor_msgs::msg::Image::SharedPtr &realsense_infra1_msg)
+    {
+        //RCLCPP_INFO(this->get_logger(), "I heard the following RealSense Infra1 message: %u. %u", realsense_infra1_msg->header.stamp.sec, realsense_infra1_msg->header.stamp.nanosec);
+
+    }
+
+    void realsense_infra2_callback(const sensor_msgs::msg::Image::SharedPtr &realsense_infra2_msg)
+    {
+        //RCLCPP_INFO(this->get_logger(), "I heard the following RealSense Infra2 message: %u. %u", realsense_infra2_msg->header.stamp.sec, realsense_infra2_msg->header.stamp.nanosec);
+
+    }
+
+    void boson_callback(const sensor_msgs::msg::Image::SharedPtr &boson_msg)
+    {
+        //RCLCPP_INFO(this->get_logger(), "I heard the following RealSense Infra2 message: %u. %u", realsense_infra2_msg->header.stamp.sec, realsense_infra2_msg->header.stamp.nanosec);
+
+    }
+
+    void approximate_sync_callback(const sensor_msgs::msg::Image::SharedPtr &boson_msg, const sensor_msgs::msg::Image::SharedPtr &realsense_rgb_msg, const sensor_msgs::msg::Image::SharedPtr &ximea_msg, const sensor_msgs::msg::Image::SharedPtr &lepton1_msg, const sensor_msgs::msg::Image::SharedPtr &lepton2_msg, const sensor_msgs::msg::Image::SharedPtr &realsense_infra1_msg, const sensor_msgs::msg::Image::SharedPtr &realsense_infra2_msg)
     {   
 
         RCLCPP_INFO(this->get_logger(), "Received APPROXIMATE msgs");
         
         // write to image using opencv
-        cv_bridge::CvImagePtr cv_ptr_rs_rgb, cv_ptr_rs_depth, cv_ptr_lepton;
-        cv_ptr_lepton = cv_bridge::toCvCopy(lepton_msg, lepton_msg->encoding);
-        cv_ptr_rs_rgb = cv_bridge::toCvCopy(realsense_rgb_msg, realsense_rgb_msg->encoding);
-        cv_ptr_rs_depth = cv_bridge::toCvCopy(realsense_depth_msg, realsense_depth_msg->encoding);
+        if (count%10==0){
+            cv_bridge::CvImagePtr cv_ptr_lepton1, cv_ptr_lepton2, cv_ptr_rs_rgb, cv_ptr_ximea, cv_ptr_rs_infra1, cv_ptr_rs_infra2, cv_ptr_boson;
+            cv_ptr_lepton1 = cv_bridge::toCvCopy(lepton1_msg, lepton1_msg->encoding);
+            cv_ptr_lepton2 = cv_bridge::toCvCopy(lepton2_msg, lepton2_msg->encoding);
+            cv_ptr_rs_rgb = cv_bridge::toCvCopy(realsense_rgb_msg, realsense_rgb_msg->encoding);
+            cv_ptr_ximea = cv_bridge::toCvCopy(ximea_msg, ximea_msg->encoding);
+            //cv_ptr_rs_depth = cv_bridge::toCvCopy(realsense_depth_msg, realsense_depth_msg->encoding);
+            cv_ptr_rs_infra1 = cv_bridge::toCvCopy(realsense_infra1_msg, realsense_infra1_msg->encoding);
+            cv_ptr_rs_infra2 = cv_bridge::toCvCopy(realsense_infra2_msg, realsense_infra2_msg->encoding);
+            cv_ptr_boson = cv_bridge::toCvCopy(boson_msg, boson_msg->encoding);
 
-        std::string img_name_lepton = ("/home/orin/m2s2_ws/Experiments/20230509/lepton_realsense/lepton/" + std::to_string(count) + ".png");
-        std::string img_name_rs_rgb = ("/home/orin/m2s2_ws/Experiments/20230509/lepton_realsense/realsense_rgb/" + std::to_string(count) + ".png");        //std::string img_name_ximea = ("/home/orin/m2s2_ws/Experiments/20230426/ximea/" + std::to_string(count) + ".png");
-        std::string img_name_rs_depth = ("/home/orin/m2s2_ws/Experiments/20230509/lepton_realsense/realsense_depth/" + std::to_string(count) + ".png");        //std::string img_name_ximea = ("/home/orin/m2s2_ws/Experiments/20230426/ximea/" + std::to_string(count) + ".png");
 
-        cv::imwrite(img_name_lepton, cv_ptr_lepton->image);
-        cv::imwrite(img_name_rs_rgb, cv_ptr_rs_rgb->image);
-        cv::imwrite(img_name_rs_depth, cv_ptr_rs_depth->image);
-    
+            std::string path = "/home/m2s2/Nutramax_Data/Calibration_Data/20230619/test2/";
+            
+
+            std::string img_name_lepton1 =   (path + "lepton1/" + std::to_string(frame_no) + ".png");
+            std::string img_name_lepton2 =   (path + "lepton2/" + std::to_string(frame_no) + ".png");
+            std::string img_name_rs_rgb =    (path + "rs_rgb/" + std::to_string(frame_no) + ".png");
+            std::string img_name_ximea  =    (path + "ximea/" + std::to_string(frame_no) + ".png");
+            //std::string img_name_rs_depth =  (path + "rs_depth/" + std::to_string(frame_no) + ".png");        
+            std::string img_name_rs_infra1 = (path + "rs_infra1/" + std::to_string(frame_no) + ".png");
+            std::string img_name_rs_infra2 = (path + "rs_infra2/" + std::to_string(frame_no) + ".png");
+            std::string img_name_boson = (path + "boson/" + std::to_string(frame_no) + ".png");
+
+
+            cv::imwrite(img_name_lepton1, cv_ptr_lepton1->image);
+            cv::imwrite(img_name_lepton2, cv_ptr_lepton2->image);
+            cv::imwrite(img_name_rs_rgb, cv_ptr_rs_rgb->image);
+            cv::imwrite(img_name_ximea, cv_ptr_ximea->image);
+            //cv::imwrite(img_name_rs_depth, cv_ptr_rs_depth->image);
+            cv::imwrite(img_name_rs_infra1, cv_ptr_rs_infra1->image);
+            cv::imwrite(img_name_rs_infra2, cv_ptr_rs_infra2->image);
+            cv::imwrite(img_name_boson, cv_ptr_boson->image);
+
+            frame_no = frame_no+1;
+        }
+
         count = count+1;
 
 
     }
 
     // Subscribers to individual topics
-    message_filters::Subscriber<sensor_msgs::msg::Image> lepton_sub_;
+    message_filters::Subscriber<sensor_msgs::msg::Image> lepton1_sub_;
+    message_filters::Subscriber<sensor_msgs::msg::Image> lepton2_sub_;
     message_filters::Subscriber<sensor_msgs::msg::Image> realsense_rgb_sub_;
-    message_filters::Subscriber<sensor_msgs::msg::Image> realsense_depth_sub_;
+    message_filters::Subscriber<sensor_msgs::msg::Image> ximea_sub_;
+    //message_filters::Subscriber<sensor_msgs::msg::Image> realsense_depth_sub_;
+    message_filters::Subscriber<sensor_msgs::msg::Image> realsense_infra1_sub_;
+    message_filters::Subscriber<sensor_msgs::msg::Image> realsense_infra2_sub_;
+    message_filters::Subscriber<sensor_msgs::msg::Image> boson_sub_;
+
 
     // Message filter stuff
-    typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::msg::Image, sensor_msgs::msg::Image, sensor_msgs::msg::Image> approximate_policy;
+    typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::msg::Image, sensor_msgs::msg::Image, sensor_msgs::msg::Image, sensor_msgs::msg::Image, sensor_msgs::msg::Image, sensor_msgs::msg::Image, sensor_msgs::msg::Image> approximate_policy;
     typedef message_filters::Synchronizer<approximate_policy> ApproxSync;
     std::shared_ptr<ApproxSync> syncApproximate;
 
     int count = 0;
+    int frame_no = 0;
 
 };
  
